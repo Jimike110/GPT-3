79e4a2c0d7b9b7db47a7d789c1c55734f0ad5fa5
Jimike <oladoyemike@gmail.com>
2023-11-07 18:50:37 +0100

    Completed 'CTA' section

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -3,8 +3,16 @@ import './cta.css';
 
 const CTA = () => {
   return (
-    <div>
-      CTA
+    <div className="gpt3__cta section__margin">
+      <div className="gpt3__cta-banner">
+        <div className="gpt3__cta-banner__text">
+          <h5>Request Early Access to Get Started</h5>
+          <h2>Register today & start exploring the endless possiblities.</h2>
+        </div>
+        <div className="gpt3__cta-banner__button">
+          <button type="button">Get Started</button>
+        </div>
+      </div>
     </div>
   )
 }
